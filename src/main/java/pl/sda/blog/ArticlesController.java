27f6ae1d134f64d563ab2532c00e28fef0e34d8a
Jakub Żulewski <j.zulewski@outlook.com>
2020-02-15 11:41:25 +0100

    Find article by id with test

--- src/main/java/pl/sda/blog/ArticlesController.java
+++ src/main/java/pl/sda/blog/ArticlesController.java
@@ -3,25 +3,31 @@ package pl.sda.blog;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ArticlesController {
     private ArticleRepository articleRepository;
-    public ArticlesController(ArticleRepository articleRepository){
+
+    public ArticlesController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
 
     @GetMapping("/articles")
-    public List<Article> getArticles(){
+    public List<Article> getArticles() {
         return articleRepository.findAll();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/articles")
-    public void postArticles(@RequestBody Article article){
-        articleRepository.save(article);
+    public Article postArticles(@RequestBody Article article) {
+        return articleRepository.save(article);
+    }
+
+    @GetMapping("/articles/{id}")
+    public Optional<Article> getArticleById(@PathVariable("id") UUID uuid) {
+        return articleRepository.findById(uuid);
     }
 }
--- src/test/java/pl/sda/blog/ArticlesControllerTest.java
+++ src/test/java/pl/sda/blog/ArticlesControllerTest.java
@@ -1,6 +1,6 @@
 package pl.sda.blog;
 
-import org.hamcrest.Matchers;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -24,6 +25,8 @@ public class ArticlesControllerTest {
     public static final String ARTICLES = "/articles";
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     // @formatter:off
     @DisplayName(
@@ -66,5 +69,45 @@ public class ArticlesControllerTest {
                 .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$[0].title", is("My First Article")));
         // @formatter:on
+    }
+
+    // @formatter:off
+    @DisplayName(
+            "given two created articles with titles 'My First article' and 'My Second Article', " +
+            "when GET /articles/id, where id is id of the first article, " +
+            "then only the first article is returned"
+    )
+    // @formatter:on
+    @Test
+    void test2() throws Exception {
+        // @formatter:off
+        // given
+        String articleToSend1 = "{ \"title\": \"My First Article\" }";
+        String articleToSend2 = "{ \"title\": \"My Second Article\" }";
+
+        // when
+        UUID idOfTheFirstArticle = createArticle(articleToSend1);
+        createArticle(articleToSend2);
+
+        // then
+        mockMvc.perform(get("/articles/{id}", idOfTheFirstArticle))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", is("My First Article")));
+        // @formatter:on
+    }
+
+    private UUID createArticle(String article) throws Exception {
+        String resultAsJson = mockMvc
+                .perform(
+                        post(ARTICLES)
+                                .content(article)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString();
+
+        Article createdArticle = objectMapper.readValue(resultAsJson, Article.class);
+
+        return createdArticle.getId();
     }
 }
